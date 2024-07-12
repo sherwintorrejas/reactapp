8499f0e35b4af276431c5f13bb930c9385dc1be1
sherwintorrejas <sherwintorrejas24@gmail.com>
2024-07-12 15:33:01 +0800

    First React app

---
+++ First_react_app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Torrejas, Sherwin C.</h1>
+        <h2>Omambac, Junmark</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
